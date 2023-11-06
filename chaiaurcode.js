52b7a52abb0af6cafac1351d0a197f3950b43296
Mohit Kaushik <mohitkaushik7737576582@gmail.com>
2023-11-06 23:51:33 +0530

    first commit

---
+++ chaiaurcode.js
@@ -0,0 +1,42 @@
+const body = document.querySelector('body')
+
+
+const randomcolor = () => {
+
+    const hex = "0123456789ABCDEF"
+    let str = "#"
+
+    for (let i = 0; i < 6; i++) {
+        const ran = Math.floor(Math.random() * 16)
+        str += hex[ran]
+    }
+
+    return str
+}
+
+const changecolor = () => {
+
+    const res = randomcolor()
+    body.style.backgroundColor = res
+
+}
+
+let func1
+
+document.getElementById('start').addEventListener('click', () => {
+
+    if(!func1)
+    {
+        func1 = setInterval(changecolor, 2000)
+    }
+
+})
+
+document.getElementById('stop').addEventListener('click', () => {
+
+    clearInterval(func1)
+
+    func1=null
+})
+
+
